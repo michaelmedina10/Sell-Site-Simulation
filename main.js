3b0b1ec9f397c440194bdb959a01ecafe37af223
Michael Medina <michael.cunha97@hotmail.com>
2022-01-23 12:04:06 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+function getElementCoordinates(element) {
+  return element.getBoundingClientRect();
+}
+
+document.querySelectorAll(".main-menu .submenu li").forEach((listItem) => {
+  listItem.onmouseover = (event) => {
+    console.log(event.target.nextElementSibling);
+    const body = document.querySelector("body");
+    const bodyCoordinate = getElementCoordinates(body);
+    if (event.target.nextElementSibling) {
+      const ulChiild = event.target.nextElementSibling;
+      const childCoordinates = getElementCoordinates(ulChiild);
+      if (childCoordinates.x > bodyCoordinate.width) {
+        console.log("É maior");
+        ulChiild.style.marginLeft = "-100.5%";
+      }
+    }
+  };
+});
+
+function ajaxNavigation(url, push = true) {
+  const mainContent = document.querySelector(".content");
+  fetch(url)
+    .then((resp) => resp.text())
+    .then((html) => (mainContent.innerHTML = html));
+
+  if (push) {
+    history.pushState({ url }, null, url);
+  }
+}
+
+document.querySelectorAll("[ajaxAttrib]").forEach((linkAjax) => {
+  console.log(linkAjax);
+  linkAjax.onclick = (event) => {
+    event.preventDefault();
+    const linkClicked = event.target;
+    ajaxNavigation(linkClicked.getAttribute("href"));
+  };
+});
+
+window.onpopstate = (event) => {
+  if (event.state) {
+    console.log(event);
+    ajaxNavigation(window.location.href, false);
+  }
+  if (event.state == null) {
+    console.log(event);
+    window.location.href = event.currentTarget.location.href;
+  }
+};
